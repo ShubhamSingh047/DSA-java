3b8e4fdb78fdd859786cc6496df571d71535ee40
ShubhamSingh047 <shubham.shubham.singh13@gmail.com>
2022-08-18 04:15:01 +0530

    updated_GCD

--- src/Lcm_Of_Two_Numb.java
+++ src/Lcm_Of_Two_Numb.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Lcm_Of_Two_Numb {
     public static void Sol(int a, int b){
         int min=Math.max(a,b); 
-        while(res>0){
-            if(res%a==0 && res%b==0) {
-                System.out.println(res);
+        while(min>0){
+            if(a%min==0 && b%min==0) {
+                System.out.println(min);
                 break;
             }
-            res-=min;
+            min--;
         }
     }
 
